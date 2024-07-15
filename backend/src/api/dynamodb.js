3fce404d9a2f36157296391705a978dfd29806c6
Kevin4153 <kevin.ayala.school415@gmail.com>
2024-07-14 20:45:58 -0700

    chaning to es module plus filling logic to fetch from table and add to table

--- backend/src/api/dynamodb.js
+++ backend/src/api/dynamodb.js
@@ -1,25 +1,43 @@
-const AWS = require('aws-sdk');
-AWS.config.update({ region: 'YOUR_REGION' });
+import AWS from 'aws-sdk';
+AWS.config.update({ region: 'us-east-2' }); // Update to your AWS region
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-const checkIngredientInDB = async (ingredient) => {
+const TABLE_NAME = 'CatToxicityInfo';
+
+export const checkIngredientInDB = async (ingredient) => {
   const params = {
-    TableName: 'YOUR_TABLE_NAME',
+    TableName: TABLE_NAME,
     Key: { 'ingredient': ingredient }
   };
   
-  const data = await docClient.get(params).promise();
-  return data.Item;
+  try {
+    const data = await docClient.get(params).promise();
+    // console.log(data.Item)
+    return data.Item;
+  } catch (err) {
+    console.error('Error checking ingredient in DB:', err);
+    throw new Error('Error checking ingredient in DB');
+  }
 };
 
-const addIngredientToDB = async (ingredient, toxicInfo) => {
+export const addIngredientToDB = async (ingredient, toxicInfo) => {
   const params = {
-    TableName: 'YOUR_TABLE_NAME',
-    Item: { 'ingredient': ingredient, ...toxicInfo }
+    TableName: TABLE_NAME,
+    Item: {
+      'ingredient': ingredient,
+    //   'type': toxicInfo.type,
+      'toxicityAmount': toxicInfo.toxicityAmount,
+      'symptoms': toxicInfo.symptoms,
+      'toxicityLevel': toxicInfo.toxicityLevel
+    }
   };
 
-  await docClient.put(params).promise();
+  try {
+    console.log(params)
+    await docClient.put(params).promise();
+  } catch (err) {
+    console.error('Error adding ingredient to DB:', err);
+    throw new Error('Error adding ingredient to DB');
+  }
 };
-
-module.exports = { checkIngredientInDB, addIngredientToDB };
