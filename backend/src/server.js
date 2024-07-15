59a19f1e3d67f881950a22b0d08a623195e5b646
Kevin4153 <kevin.ayala.school415@gmail.com>
2024-07-14 20:40:45 -0700

    adding cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,15 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const foodItemRoutes = require('./routes/foodItemRoutes');
-const toxicityRoutes = require('./routes/toxicityRoutes');
+import dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import foodItemRoutes from './routes/foodItemRoutes.js';
+import toxicityRoutes from './routes/toxicityRoutes.js';
+
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors()); 
 
 app.use('/api/foodItem', foodItemRoutes);
 app.use('/api/toxicity', toxicityRoutes);
