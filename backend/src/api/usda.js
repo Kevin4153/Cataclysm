05eb32cde89083b02ed3c52d212eed5baa485293
Kevin4153 <kevin.ayala.school415@gmail.com>
2024-07-14 20:42:33 -0700

    chaning to es modules plus adding logic to handle api call to usda

--- backend/src/api/usda.js
+++ backend/src/api/usda.js
@@ -1,8 +1,25 @@
-const axios = require('axios');
+import axios from 'axios';
+import dotenv from "dotenv";
+
+dotenv.config()
+
 
 const getFoodItem = async (barcode) => {
-  const response = await axios.get(`https://api.usda.gov/fdc/v1/foods/search?query=${barcode}&pageSize=10&api_key=${process.env.USDA_API_KEY}`);
-  return response.data;
+  try {
+    const response = await axios.get(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${barcode}&pageSize=10&api_key=${process.env.USDA_API_KEY}`);
+    if (response.data && response.data.foods && response.data.foods.length > 0) {
+        const foodItem = response.data.foods[0];
+        // Split ingredients string into an array
+        foodItem.ingredients = foodItem.ingredients ? foodItem.ingredients.split(',').map(ingredient => ingredient.trim()) : [];
+        return foodItem;    
+    } else {
+        console.log('No food item found')
+        return null; // No food items found
+    }
+    } catch (error) {
+        console.error('Error fetching food item:', error);
+        return null; // Return null in case of an error
+    }
 };
 
-module.exports = { getFoodItem };
+export default getFoodItem
